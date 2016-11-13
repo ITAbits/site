59489d508147706635cebb8a62074d23e05bdff4
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-11-13 04:49:03 -0200

    gh-pages fixing

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
-var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'build');
+var BUILD_DIR = path.resolve(__dirname, 'public', 'build');
 var APP_DIR = path.resolve(__dirname, '');
 
 var config = {
