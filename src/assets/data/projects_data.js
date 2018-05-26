ab1b24bcdf4b0c4a8d7f2cd28bef504e6ebb518d
igor-ribeiiro <ericpqmor@gmail.com>
2018-05-26 20:57:34 -0300

    Adding project gifs

--- src/assets/data/projects_data.js
+++ src/assets/data/projects_data.js
@@ -1,12 +1,16 @@
-import balance from '../images/projects/balance.png'
-import overhead from '../images/projects/overhead.png'
-import ricochette from '../images/projects/ricochette.png'
+import balanceimg from '../images/projects/balance.png'
+import overheadimg from '../images/projects/overhead.png'
+
+import balancegif from '../images/projects/balance.gif'
+import overheadgif from '../images/projects/overhead.gif'
+//import ricochette from '../images/projects/ricochette.png'
 
 export default [
   {
     name: 'Balance',
     info: 'Balance is a simple and challenging arcade game made using Pygame, inspired on the mobile game Duet.',
-    img: balance,
+    img: balanceimg,
+    gif: balancegif,
     alt: 'Balance',
     developers: 'Carlos Matheus, Eric Moreira and Igor Ribeiro',
     codeLink: 'https://github.com/CarlosMatheus/Balance',
@@ -15,19 +19,20 @@ export default [
   {
     name: 'Overhead',
     info: 'OverHead is a tower defense 3D game with focus on strategy. You can get souls from defeated enemies to generate energy, summon towers to defend the paths that lead to your Master Tower or upgrade your powers in order to make an efficient strategy. Can you survive? Caution is needed, you are very hated and will be attacked from all directions! Try to stay in the lead and make your name!',
-    img: overhead,
+    img: overheadimg,
+    gif: overheadgif,
     alt: 'Overhead',
     developers: 'Carlos Matheus, Francisco Castro and Luiz Henrique Aguiar',
     codeLink: 'https://github.com/CarlosMatheus/Overhead',
     demoLink: 'https://chicomcastro.itch.io/overhead'
-  },
-  {
-    name: 'Ricochette',
-    info: 'Ricochette is a two-player shooter game. Play against your friends and remember... the more you shoot, the worst it is not just for your opponent, but for yourself as well.',
-    img: ricochette,
-    alt: 'Ricochette',
-    developers: 'Felipe Guimarães',
-    codeLink: 'https://github.com/guimafelipe/ricochette',
-    demoLink: 'https://www.kongregate.com/games/FlpGuima/ricochette'
   }
+  // {
+  //   name: 'Ricochette',
+  //   info: 'Ricochette is a two-player shooter game. Play against your friends and remember... the more you shoot, the worst it is not just for your opponent, but for yourself as well.',
+  //   img: ricochette,
+  //   alt: 'Ricochette',
+  //   developers: 'Felipe Guimarães',
+  //   codeLink: 'https://github.com/guimafelipe/ricochette',
+  //   demoLink: 'https://www.kongregate.com/games/FlpGuima/ricochette'
+  // }
 ];
