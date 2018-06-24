035e95cc42fb3c8d3dd318dd4e17ce470db4c408
Igor Bragaia <igor.bragaia@gmail.com>
2018-06-23 22:14:59 -0300

    header style fix

--- src/components/header.js
+++ src/components/header.js
@@ -3,50 +3,47 @@ import React from 'react'
 
 export default props => {
   return (
-    <div id='header-container'>
-        <svg class="animate-reveal animate-second" width="100vw" height="100vh">
+    <div id='header-container' style={{textAlign: 'center'}}>
+        <svg class="animate-reveal animate-second" width="100vw" height="70vh">
           <defs>
             <symbol id="red">
-              <rect width="100" height="100" style={{fill:'rgb(211,47,47)'}}/>
-              <rect y="100" width="100" height="9" style={{fill:'rgb(183,28,28)'}}/>
+              <rect width="50" height="50" style={{fill:'rgb(211,47,47)'}}/>
+              <rect y="50" width="50" height="9" style={{fill:'rgb(183,28,28)'}}/>
             </symbol>
             <symbol id="yellow">
-              <rect width="100" height="100" style={{fill:'rgb(255,193,7)'}}/>
-              <rect y="100" width="100" height="9" style={{fill:'rgb(245,127,23)'}}/>
+              <rect width="50" height="50" style={{fill:'rgb(255,193,7)'}}/>
+              <rect y="50" width="50" height="9" style={{fill:'rgb(245,127,23)'}}/>
             </symbol>
             <symbol id="blue">
-              <rect width="100" height="100" style={{fill:'rgb(63,81,181)'}}/>
-              <rect y="100" width="100" height="9" style={{fill:'rgb(26,35,126)'}}/>
+              <rect width="50" height="50" style={{fill:'rgb(63,81,181)'}}/>
+              <rect y="50" width="50" height="9" style={{fill:'rgb(26,35,126)'}}/>
             </symbol>
           </defs>
-          <g transform="translate(300, 50)">
-            <g>
-              <use xlinkHref="#red"/>
-              <g transform="translate(0, 62)">
-                <use xlinkHref="#yellow" x="-212"/>
-                <use xlinkHref="#blue"   x="-106"/>
-                <use xlinkHref="#blue"   x="106"/>
-                <use xlinkHref="#yellow" x="212"/>
-                <g transform="translate(0, 112)">
-                  <use xlinkHref="#yellow" x="-106"/>
-                  <use xlinkHref="#blue"/>
-                  <use xlinkHref="#yellow" x="106"/>
-                  <g transform="translate(0, 112)">
-                    <use xlinkHref="#yellow"/>
+          <g>
+            <g style={{transform: 'translate(50%, 5%)'}}>
+              <g transform="translate(-27.5,0)">
+                <use xlinkHref="#red"/>
+                <g transform="translate(0,35)">
+                  <use xlinkHref="#yellow" x="-110"/>
+                  <use xlinkHref="#blue"   x="-55"/>
+                  <use xlinkHref="#blue"   x="55"/>
+                  <use xlinkHref="#yellow" x="110"/>
+                  <g transform="translate(0, 64)">
+                    <use xlinkHref="#yellow" x="-55"/>
+                    <use xlinkHref="#blue"/>
+                    <use xlinkHref="#yellow" x="55"/>
+                    <g transform="translate(0, 64)">
+                      <use xlinkHref="#yellow"/>
+                    </g>
                   </g>
                 </g>
               </g>
             </g>
-            <g transform="translate(-80, 400)">
-              <path id="path">
-              <animate attributeName="d" from="m0,110 h0" to="m0,110 h1100" dur="6.8s" begin="1.2s" fill="freeze"/>
-              </path>
-              <text font-size="72" font-family="Montserrat" style={{fill:'rgb(255, 255, 255)'}}>
-                <textPath xlinkHref="#path">itabits</textPath>
-              </text>
-            </g>
           </g>
         </svg>
+        <div class="typewriter">
+          <h1>itabits</h1>
+        </div>
     </div>
   )
 }
