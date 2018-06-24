4e7f98dd3333ac177e2ecacd6536fab14d372783
Eric Moreira <ericpqmor@gmail.com>
2018-06-24 00:02:29 -0300

    Changing about style

--- src/components/about.js
+++ src/components/about.js
@@ -1,54 +1,26 @@
 import React from 'react'
 import { Grid, Container, List } from 'semantic-ui-react'
 
+import '../style/About.css'
 
 export default props => {
   return (
-    <div style={{color:'white'}}>
-      <Container textAlign='center'>
-          <Grid centered columns={2} verticalAlign='middle'>
-            <Grid.Row centered columns={2}>
-              <Grid.Column>
-                <p class="animate-reveal animate-first">
-                    <h4>Missão</h4>
-                    Centralizar o desenvolvimento de software em um ambiente onde as pessoas possam aprender e produzir juntas.
-                  </p>
-              </Grid.Column>
-              <Grid.Column>
-                <p class="animate-reveal animate-first">
-                    <h4>Visão</h4>
-                    Tornar-se um centro de tecnologia e entretenimento.
-                  </p>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row centered columns={2}>
-              <Grid.Column>
-                <p class="animate-reveal animate-second">
-                <h4>Valores</h4>
-                    <List>
-                      <List.Item>Perpetuar o conhecimento</List.Item>
-                      <List.Item>Divirta-se</List.Item>
-                      <List.Item>Respeito mútuo</List.Item>
-                      <List.Item>Siga as regras do jogo</List.Item>
-                      <List.Item>Valorize o trabalho em equipe</List.Item>
-                      <List.Item>Há sempre o que aprender</List.Item>
-                      <List.Item>Não moque o bizu</List.Item>
-                      <List.Item>Gostoso é melhor que grande</List.Item>
-                      <List.Item>Busque resultados</List.Item>
-                    </List>
-                </p>
-              </Grid.Column>
-              <Grid.Column>
-                <p  class="animate-reveal animate-second">
-                <h4>Um pouco de História</h4>
-                  Tudo começou no final de 2008 com um grupo de amigos da turma 12, com o objetivo de participar do Simpósio Brasileiro de Jogos e Entretenimento Digital (SBGames). Em 2009, o projeto foi inserido na disciplina MTP-02 do ITA, demonstrando o apoio da escola. Paralelamente, um dos veteranos que tinha ganhado essa mesma competição em 2007 estava auxiliando o grupo de segundanistas. O projeto ficou em 1º lugar na Feira de Ciências do ITA.
-                <br/>
-                  No fim de 2009, o grupo decidiu expandir e criar uma iniciativa para competicões na área de jogos eletrônicos a exemplo da Aerodesign e Baja. Com a divulgação da idéia, um membro que era da antiga ITAndroids (iniciativa voltado para competições de robótica) trouxe a sugestão de criar uma iniciativa que abrangesse diversos projetos de Computação voltados para competições. Dado a interseccão de muitos temas entre as áreas na Computação, o que permite trocas de experiências e auxílio entre os membros, a sugestão foi acatada e foi dado início oficial à iniciativa.
-                </p>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
-    </div>
+    <Container id='about-container'>
+      <h3>QUEM SOMOS?</h3>
+      <hr />
+      <p>
+          A ITA Bits é um grupo de desenvolvimento de software criado e mantido por alunos de graduação do Instituto Tecnológico de Aeronáutica (ITA).
+          O objetivo da equipe é centralizar o desenvolvimento de software em um ambiente onde as pessoas possam aprender e produzir juntas.
+          Os membros da ITA Bits participam de diversas competições, como a Olimpíada Brasileira de Informática (OBI) e a Maratona de Programação, além de desenvolverem
+          jogos, sites e outros. A ITA Bits realiza também todos os anos a CIG (Competição Interna de Games do ITA), uma competição focada em promover o desenvolvimento de jogos feitos por alunos
+          de graduação de todos os anos. Além disso, todos os anos a equipe organiza treinamentos voltados para programação competitiva e desenvolvimento de jogos.
+      </p>
+      <p>
+          A ITA Bits foi criada em 2008 com um grupo de amigos da Turma 2012, com o objetivo de participar do Simpósio Brasileiro de Jogos e Entretenimento Digital (SBGames).
+          Em 2009, o projeto foi inserido na disciplina MTP-02 (Introdução à Engenharia) do ITA, demonstrando apoio da escola. Paralelamente, um dos veteranos que havia ganhado
+          essa mesma competição em 2007 estava auxiliando o grupo de segundanistas. O projeto ganhou a Feira de Ciências do ITA. Em 2009, esse grupo de amigos levou o projeto
+          para frente: assim surgia a ITA Bits.
+      </p>
+    </Container>
   )
 }
