026dc5cc972e7ebc4e029d68d1c613a2c4518b9c
Eric Moreira <ericpqmor@gmail.com>
2018-06-24 03:25:25 -0300

    Adding info sector

---
+++ src/components/InfoSector.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Grid, Container, List } from 'semantic-ui-react'
+
+import '../style/Info.css'
+
+export default props => {
+  return (
+    <div id='info-container'>
+      <section id='info-section'>
+        <h3>NOSSAS ATIVIDADES</h3>
+        <hr />
+        <Grid centered columns={3} verticalAlign='middle'>
+          <Grid.Row centered columns={3}>
+            <Grid.Column centered>
+              <i className='fa fa-trophy' />
+            </Grid.Column>
+
+            <Grid.Column>
+              <i className='fa fa-gamepad' />
+            </Grid.Column>
+
+            <Grid.Column>
+              <i className='fa fa-comments' />
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row centered columns={3}>
+            <Grid.Column centered>
+              <h5>Competições</h5>
+            </Grid.Column>
+
+            <Grid.Column>
+              <h5>Games</h5>
+            </Grid.Column>
+
+            <Grid.Column>
+              <h5>Treinamentos</h5>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+        <Grid columns={3} verticalAlign='justify'>
+          <Grid.Row centered columns={3}>
+            <Grid.Column>
+              <p>Todos os anos, nossa equipe participa das principais competições de programação do Brasil e do mundo: Olimpíada Brasileira de Informática (OBI), Maratona de Programação, Google Code Jam e TopCoder Open. Também produzimos simulados semanais para incentivar o aprendizado de programação no ITA.</p>
+            </Grid.Column>
+
+            <Grid.Column>
+              <p>A equipe da ITABits também incentiva e participa diversas competições de games, como Game Jams e Ludum Dares. Além disso, também organizamos todos os anos a Competição Interna de Games (CIG) do ITA, como forma de incentivar alunos de graduação a iniciar no desenvolvimento de jogos.</p>
+            </Grid.Column>
+
+            <Grid.Column>
+              <p>Nossa equipe também fornece treinamentos sobre computação dentro do ITA. Os treinamentos abrangem tópicos básicos, como introdução à programação em C, dois cursos (Básico e Avançado) sobre algoritmos e estruturas de dados e diversos cursos de game development com C# e Unity.</p>
+            </Grid.Column>
+
+          </Grid.Row>
+        </Grid>
+      </section>
+    </div>
+  )
+}
